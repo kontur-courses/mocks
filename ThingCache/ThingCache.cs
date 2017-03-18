5e404add7f2ea93d7c4ab00fb5d4d9c8104d5b89
Pavel Egorov <xoposhiy@mail.ru>
2017-03-18 12:39:16 +0500

    small fixes

--- ThingCache/ThingCache.cs
+++ ThingCache/ThingCache.cs
@@ -3,51 +3,51 @@ using NUnit.Framework;
 
 namespace MockFramework
 {
-    public class ThingCache
-    {
-        private readonly IDictionary<string, Thing> dictionary
-            = new Dictionary<string, Thing>();
-        private readonly IThingService thingService;
+	public class ThingCache
+	{
+		private readonly IDictionary<string, Thing> dictionary
+			= new Dictionary<string, Thing>();
+		private readonly IThingService thingService;
 
-        public ThingCache(IThingService thingService)
-        {
-            this.thingService = thingService;
-        }
+		public ThingCache(IThingService thingService)
+		{
+			this.thingService = thingService;
+		}
 
-        public Thing Get(string thingId)
-        {
-            Thing thing;
-            if (dictionary.TryGetValue(thingId, out thing))
-                return thing;
-            if (thingService.TryRead(thingId, out thing))
-            {
-                dictionary[thingId] = thing;
-                return thing;
-            }
-            return null;
-        }
-    }
+		public Thing Get(string thingId)
+		{
+			Thing thing;
+			if (dictionary.TryGetValue(thingId, out thing))
+				return thing;
+			if (thingService.TryRead(thingId, out thing))
+			{
+				dictionary[thingId] = thing;
+				return thing;
+			}
+			return null;
+		}
+	}
 
-    [TestFixture]
-    public class ThingCache_Should
-    {
-        private IThingService thingService;
-        private ThingCache thingCache;
+	[TestFixture]
+	public class ThingCache_Should
+	{
+		private IThingService thingService;
+		private ThingCache thingCache;
 
-        private const string thingId1 = "TheDress";
-        private Thing thing1 = new Thing(thingId1);
+		private const string thingId1 = "TheDress";
+		private Thing thing1 = new Thing(thingId1);
 
-        private const string thingId2 = "CoolBoots";
-        private Thing thing2 = new Thing(thingId2);
+		private const string thingId2 = "CoolBoots";
+		private Thing thing2 = new Thing(thingId2);
 
-        [SetUp]
-        public void SetUp()
-        {
-            //thingService = A...
-            thingCache = new ThingCache(thingService);
-        }
+		[SetUp]
+		public void SetUp()
+		{
+			//thingService = A...
+			thingCache = new ThingCache(thingService);
+		}
 
-        //TODO: написать простейший тест, а затем все остальные
-        //Live Template tt работает!
-    }
+		//TODO: написать простейший тест, а затем все остальные
+		//Live Template tt работает!
+	}
 }
