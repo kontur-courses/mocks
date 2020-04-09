39b39a6259f7bd2c4af5ea05a6ee043bdf9e6d61
alexiustim <alexiustim@gmail.com>
2020-04-09 18:31:44 +0500

    ThingCache tests

--- ThingCache/ThingCache.cs
+++ ThingCache/ThingCache.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections.Generic;
-using NUnit.Framework;
 
 namespace MockFramework
 {
     public class ThingCache
     {
-        private readonly IDictionary<string, Thing> dictionary
-            = new Dictionary<string, Thing>();
+        private readonly IDictionary<string, Thing> dictionary = new Dictionary<string, Thing>();
         private readonly IThingService thingService;
 
         public ThingCache(IThingService thingService)
@@ -16,38 +14,12 @@ namespace MockFramework
 
         public Thing Get(string thingId)
         {
-            Thing thing;
-            if (dictionary.TryGetValue(thingId, out thing))
-                return thing;
-            if (thingService.TryRead(thingId, out thing))
-            {
-                dictionary[thingId] = thing;
-                return thing;
-            }
-            return null;
+            if (dictionary.TryGetValue(thingId, out var thing)) return thing;
+            if (!thingService.TryRead(thingId, out thing)) return null;
+
+            dictionary[thingId] = thing;
+            
+            return thing;
         }
-    }
-
-    [TestFixture]
-    public class ThingCache_Should
-    {
-        private IThingService thingService;
-        private ThingCache thingCache;
-
-        private const string thingId1 = "TheDress";
-        private Thing thing1 = new Thing(thingId1);
-
-        private const string thingId2 = "CoolBoots";
-        private Thing thing2 = new Thing(thingId2);
-
-        [SetUp]
-        public void SetUp()
-        {
-            //thingService = A...
-            thingCache = new ThingCache(thingService);
-        }
-
-        //TODO: написать простейший тест, а затем все остальные
-        //Live Template tt работает!
     }
 }
---
+++ ThingCache/ThingCacheTests.cs
@@ -0,0 +1,76 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+
+namespace MockFramework
+{
+    [TestFixture]
+    public class ThingCacheTests
+    {
+        private IThingService thingService;
+        private ThingCache thingCache;
+
+        private const string thingId1 = "TheDress";
+        private static readonly Thing thing1 = new Thing(thingId1);
+
+        private const string thingId2 = "CoolBoots";
+        private static Thing thing2 = new Thing(thingId2);
+
+        private static object[] ValidIdentifiers =
+        {
+            new object[] {thingId1, thing1},
+            new object[] {thingId2, thing2}
+        };
+
+        private static object[] WrongIdentifiers =
+        {
+            new object[] {""},
+            new object[] {"someID"}
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            thingService = A.Fake<IThingService>();
+
+            Thing t;
+            A.CallTo(() => thingService.TryRead(thingId1, out t)).Returns(true).AssignsOutAndRefParameters(thing1);
+            A.CallTo(() => thingService.TryRead(thingId2, out t)).Returns(true).AssignsOutAndRefParameters(thing2);
+
+            thingCache = new ThingCache(thingService);
+        }
+
+        [Test, TestCaseSource(nameof(WrongIdentifiers))]
+        public void Get_ReturnsNullByUnknownIdOrBadId(string thingId)
+        {
+            var t = thingCache.Get(thingId);
+            Assert.That(t, Is.Null);
+        }
+
+        [Test, TestCaseSource(nameof(ValidIdentifiers))]
+        public void Get_ReturnsThingByThingId(string thingId, Thing thing)
+        {
+            var t = thingCache.Get(thingId);
+            Assert.That(t, Is.EqualTo(thing));
+        }
+
+        [Test]
+        public void Get_CallsTryReadForFirstTry()
+        {
+            thingCache.Get(thingId1);
+            Thing t;
+            A.CallTo(() => thingService.TryRead(A<string>.Ignored, out t)).MustHaveHappened();
+        }
+
+        [TestCase(thingId1, 10)]
+        public void Get_ReturnsFromCacheForOthers(string thingId, int tryCount)
+        {
+            for (var i = 0; i < tryCount; i++)
+            {
+                thingCache.Get(thingId);
+            }
+            
+            Thing t;
+            A.CallTo(() => thingService.TryRead(thingId1, out t)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+    }
+}
