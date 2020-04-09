74e42c80d2f668c8da81196a7253ee61e86d02a0
alexiustim <alexiustim@gmail.com>
2020-04-09 19:18:47 +0500

    remark corrections

--- ThingCache/ThingCacheTests.cs
+++ ThingCache/ThingCacheTests.cs
@@ -1,4 +1,5 @@
-﻿using FakeItEasy;
+﻿using System;
+using FakeItEasy;
 using NUnit.Framework;
 
 namespace MockFramework
@@ -10,7 +11,7 @@ namespace MockFramework
         private ThingCache thingCache;
 
         private const string thingId1 = "TheDress";
-        private static readonly Thing thing1 = new Thing(thingId1);
+        private static Thing thing1 = new Thing(thingId1);
 
         private const string thingId2 = "CoolBoots";
         private static Thing thing2 = new Thing(thingId2);
@@ -32,18 +33,24 @@ namespace MockFramework
         {
             thingService = A.Fake<IThingService>();
 
-            Thing t;
-            A.CallTo(() => thingService.TryRead(thingId1, out t)).Returns(true).AssignsOutAndRefParameters(thing1);
-            A.CallTo(() => thingService.TryRead(thingId2, out t)).Returns(true).AssignsOutAndRefParameters(thing2);
+            A.CallTo(() => thingService.TryRead(thingId1, out thing1)).Returns(true);
+            A.CallTo(() => thingService.TryRead(thingId2, out thing2)).Returns(true);
 
             thingCache = new ThingCache(thingService);
         }
 
         [Test, TestCaseSource(nameof(WrongIdentifiers))]
-        public void Get_ReturnsNullByUnknownIdOrBadId(string thingId)
+        public void Get_ReturnsNullByUnknownId(string thingId)
         {
             var t = thingCache.Get(thingId);
             Assert.That(t, Is.Null);
+        }
+        
+        [Test]
+        public void Get_ThrowsExceptionByBadId()
+        {
+            var exception = Assert.Catch<Exception>(() => thingCache.Get(null));
+            Assert.That(exception, Is.TypeOf<ArgumentNullException>());
         }
 
         [Test, TestCaseSource(nameof(ValidIdentifiers))]
@@ -62,7 +69,7 @@ namespace MockFramework
         }
 
         [TestCase(thingId1, 10)]
-        public void Get_ReturnsFromCacheForOthers(string thingId, int tryCount)
+        public void Get_CallsOnceTryReadForOthers(string thingId, int tryCount)
         {
             for (var i = 0; i < tryCount; i++)
             {
@@ -71,6 +78,24 @@ namespace MockFramework
             
             Thing t;
             A.CallTo(() => thingService.TryRead(thingId1, out t)).MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [TestCase(thingId1)]
+        public void Get_ReturnsFromCacheForOthers(string thingId)
+        {
+            var firstThing = thingCache.Get(thingId);
+            var secondThing = thingCache.Get(thingId);
+            
+            Assert.That(firstThing, Is.EqualTo(secondThing));
+        }
+        
+        [Test]
+        public void Get_ReturnsFromCacheForDifferentThings()
+        {
+            var firstThing = thingCache.Get(thingId1);
+            var secondThing = thingCache.Get(thingId2);
+            
+            Assert.That(firstThing, Is.Not.EqualTo(secondThing));
         }
     }
 }
