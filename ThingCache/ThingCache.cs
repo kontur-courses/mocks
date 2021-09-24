2c6e97970faf9e200e5bd87c03f8e3643ddc11a3
sit@332lab.ru <sit@332lab.ru>
2021-09-24 10:51:56 +0700

    дописал тесты для ThingCache

--- ThingCache/ThingCache.cs
+++ ThingCache/ThingCache.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using FakeItEasy;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace MockFramework
@@ -44,18 +46,88 @@ namespace MockFramework
         [SetUp]
         public void SetUp()
         {
-            //thingService = A...
+            thingService = A.Fake<IThingService>();
+            
+            Thing _;
+            A.CallTo(() => thingService.TryRead(thingId1, out _))
+                .Returns(true)
+                .AssignsOutAndRefParameters(thing1);
+
+            A.CallTo(() => thingService.TryRead(thingId2, out _))
+                .Returns(true)
+                .AssignsOutAndRefParameters(thing2);
+            
             thingCache = new ThingCache(thingService);
         }
 
-        // TODO: Написать простейший тест, а затем все остальные
-        // Live Template tt работает!
-
-        // Пример теста
         [Test]
-        public void GiveMeAGoodNamePlease()
+        public void Get_NotExistingThing_ReturnsNull_CallServiceOnce()
         {
+            // Act
+            var notExistingThingId = "NotExistingThing";
+            var thing = thingCache.Get(notExistingThingId);
+            
+            // Assert
+            thing.Should().BeNull();
+            Thing _;
+            A.CallTo(() => thingService.TryRead(notExistingThingId, out _)).MustHaveHappenedOnceExactly();
         }
+
+        [Test]
+        public void DoubleGet_NotExistingThing_ReturnsNull_CallServiceTwice()
+        {
+            // Act
+            var notExistingThingId = "NotExistingThing";
+            var firstTry = thingCache.Get(notExistingThingId);
+            var secondTry = thingCache.Get(notExistingThingId);
+            
+            // Assert
+            firstTry.Should().BeNull();
+            secondTry.Should().BeNull();
+            
+            Thing _;
+            A.CallTo(() => thingService.TryRead(notExistingThingId, out _)).MustHaveHappenedTwiceExactly();
+        }
+
+        [Test]
+        public void Get_ExistingThing1_ReturnsCorrectThing_CallServiceOnce()
+        {
+            // Act
+            var thing = thingCache.Get(thingId1);
+            
+            // Assert
+            thing.Should().Be(thing1);
+            Thing _;
+            A.CallTo(() => thingService.TryRead(thingId1, out _)).MustHaveHappenedOnceExactly();
+        }
+        
+        [Test]
+        public void DoubleGet_ExistingThing1_ReturnsCorrectThing_CallServiceOnce()
+        {
+            // Act
+            var firstTry = thingCache.Get(thingId1);
+            var secondTry = thingCache.Get(thingId1);
+            
+            // Assert
+            firstTry.Should().Be(thing1);
+            secondTry.Should().Be(thing1);
+            
+            Thing _;
+            A.CallTo(() => thingService.TryRead(thingId1, out _)).MustHaveHappenedOnceExactly();
+        }
+        
+        [Test]
+        public void Get_ExistingThing2_ReturnsCorrectThing_CallServiceOnce()
+        {
+            // Act
+            var thing = thingCache.Get(thingId2);
+            
+            // Assert
+            thing.Should().Be(thing2);
+            Thing _;
+            A.CallTo(() => thingService.TryRead(thingId2, out _)).MustHaveHappenedOnceExactly();
+        }
+        
 
         /** Проверки в тестах
          * Assert.AreEqual(expectedValue, actualValue);
